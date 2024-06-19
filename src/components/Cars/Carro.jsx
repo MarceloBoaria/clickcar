cf69e7f030eeb1f2f300e85e74fd3ff33af9baa7
gabriellemosDev <gabrielroocha1@gmail.com>
2024-06-18 22:13:17 -0300

    feito as cards, colocado foto, mexi no sobre, mexi em outras coisas

--- src/components/Cars/Carro.jsx
+++ src/components/Cars/Carro.jsx
@@ -1,29 +1,43 @@
 import { useEffect, useState } from "react";
 import "./Carro.css";
+import WhatsApp from '../WhatsApp/WhatsApp';
 
 function Carro(props) {
-    
-    const [marca, setMarca] = useState(props.marca);
+  const [marca, setMarca] = useState(props.marca);
 
-    useEffect(() => {
-    }, [marca]);
+  useEffect(() => {}, [marca]);
 
-    return (
-        <div className="divCarro">
-            <div className="divParagrafo">
-                <h2>Marca: {marca}</h2>
-                <p className="pDivCarro">Modelo: {props.modelo}</p>
-                <p className="pDivCarro">Ano: {props.ano}</p>
-            </div>
-            <div className="divImagem">
-                <label>Imagem Carro</label>
-            </div>
-            <div className="divBtnValores">
-                <button className="btnValores">Valor Original</button>
-                <button className="btnValores">Promocional</button>
-            </div>
-        </div>
-    );
+  return (
+
+    <div className="divCarro">
+      {/* <class>{props.image}</class> */}
+      <div className="sub_carro">
+        <img className="foto" src={props.image} alt="Carro" />
+      </div>
+
+      <div className="NomeCarro">
+        <p>{props.marca}</p>
+      </div>
+      <div className="DescriçãoCarro">
+        <p>{props.modelo}</p>
+      </div>
+      <div className="Preço">
+        <p>{props.preco}</p>
+      </div>
+      <div className="DataCarro">
+        <p>{props.ano} </p>
+      </div>
+      <div className="KmCarro">
+        <p>{props.km}</p>
+      </div>
+      
+      <div type="text" placeholder="SAIBA MAIS" className="carro"  >
+      <WhatsApp />
+      </div>
+      <hr className="Line"></hr>
+      <p className="Localizacao">{props.localizacao}</p>
+    </div>
+  );
 }
 
 export default Carro;
