1cb046ea44d18cd49ae310da7170ad906ed00669
Marcelo <marceloboaria@gmail.com>
2024-06-20 21:40:15 -0300

    atualizações de cadastro e login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { UsuariosContextProvider } from './contexts/GlobalContext';
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
-// import BackgroundVideo from './components/ImageCar/BackgroundVideo';
 
 function App() {
   return (
